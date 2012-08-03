0c65b315b5cb79dc113cb7e81ca493b011f9cb82
tomekl <tomasz.lipowicz@gmail.com>
2012-08-03 14:29:21 +0100

    multiple cars allowed on same page

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,84 +1,88 @@
 (function( $ ){
 	var methods = {
-	o:{
-		'direction': 'horizontal',
-		'animation_time':500,
-		'slides':'.items',
-		'slide_start': 0
-	},
     init : function( options ) { 
-		console.log(this)
-		if(options){
-			methods.o = $.extend(methods.o, options);
+		var s = {
+			'direction': 'horizontal',
+			'animation_time':500,
+			'slides':'.items',
+			'slide_start': 0
 		}
-		methods.clicks(this);
-		methods.build(this);
+		if(options){
+			o = $.extend(s, options);
+		}
+		methods.clicks(this, o);
+		methods.build(this, o);
     },
-    build : function(el) {
+    build : function(el, o) {
 		pos = [];
 		var top = 0
-		if(this.o.direction == "horizontal"){
+		if(o.direction == "horizontal"){
 			$('#'+el.attr("id")+' .items').css({"float":"left"})
 			$('#'+el.attr("id")+' .i_wrap').css({"width":(el.height()*$('.items').length)+"px"})
 		}
 		el.css({'position':'relative', 'overflow':'hidden'})
-		el.addClass(this.o.direction);
+		el.addClass(o.direction);
 		var el_height = el.height() - (parseFloat($(".items").css('padding-top'),10) + parseFloat($(".items").css('padding-bottom'),10));
 		var el_width = el.width() - (parseFloat($(".items").css('padding-left'),10) + parseFloat($(".items").css('padding-right'),10));
 		$('#'+el.attr("id")+' .items').css({"height":el_height+'px', "width":el_width+'px'});
 		$('.items').each(function(i,e){
 			$(this).attr("id", $(this).attr("class")+"_"+i);
 		})
-		$(this.html.nav[this.o.direction]).attr("data-owner",el.attr("id")).prependTo(el);
+		$(this.html.nav[o.direction]).attr("data-owner",el.attr("id")).prependTo(el);
     },
-	animate : function(dir, el){
-		console.log(el.height());
-		console.log(el.width());
+	animate : function(dir, el, o){
 		var a_el = '#'+el.attr('id')+" .i_wrap";
-		console.log(methods.o.slide_start);
-		console.log(this)
+
 		switch(dir){
 			case 'up':
-				if(this.o.slide_start <= ($(a_el+' .items').length-2)){
+				console.log(o.slide_start);
+				if(o.slide_start <= ($(a_el+' .items').length-2)){
 					($(a_el)).animate({
 						top:'-='+el.height()
-					}, this.o.animation_time);
-					this.o.slide_start = this.o.slide_start +1;
+					}, o.animation_time);
+					o.slide_start = o.slide_start +1;
+					console.log(o.slide_start);
 				}
 			break;
 			case 'down':
-				if(this.o.slide_start >= 1){
+				console.log(o.slide_start);
+				if(o.slide_start >= 1){
 					($(a_el)).animate({
 						top:'+='+el.height()
-					}, this.o.animation_time);
-				this.o.slide_start = this.o.slide_start -1;
+					}, o.animation_time);
+					o.slide_start = o.slide_start -1;
+					console.log(o.slide_start);
 				}	
 			break;
 			case "left":
-				if(this.o.slide_start >= 1){
+				console.log(o.slide_start);
+				if(o.slide_start >= 1){
 					($(a_el)).animate({
 						left:'+='+el.width()
-					}, this.o.animation_time);
-					this.o.slide_start = this.o.slide_start -1;
-					console.log(this.o.slide_start)
+					}, o.animation_time);
+					o.slide_start = o.slide_start -1;
+					console.log(o.slide_start);
 				}
 			break;
 			case "right":
-				if(this.o.slide_start <= ($(a_el+' .items').length-2)){
+				console.log(o.slide_start);
+				if(o.slide_start <= ($(a_el+' .items').length-2)){
 					($(a_el)).animate({
 						left:'-='+el.width()
-					}, this.o.animation_time);
-					this.o.slide_start = this.o.slide_start +1;
+					}, o.animation_time);
+					o.slide_start = o.slide_start +1;
+					console.log(o.slide_start);
 				}	
 			break
 		}
 	},
-	clicks : function(el){
+	clicks : function(el, o){
+		console.log(o);
 		$(el).on('click', 'a.btn', function(e){
+			console.log(o)
+			console.log(el);
 			e.preventDefault();
-			console.log(this)
-			//methods.animate($(this).data().action , $(this).parent().data().owner)
-			methods.animate($(this).data().action , el)
+			methods.animate($(this).data().action , el, o)
 		})
 	},
 	html : {
